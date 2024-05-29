c2dbc1d6e5942fdf038fb21e4208c873c5653246
unknown <andressa7090@gmail.com>
2024-05-29 11:18:00 -0300

    em andamento

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Text, StyleSheet, View, Image,FlatList} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './Screens/Home';
+import Details from './Screens/Details';
 export default function App() {
 
   const stack = createStackNavigator();
@@ -13,7 +14,7 @@ export default function App() {
         <stack.Navigator>
           
           <stack.Screen name='Home' component={Home}></stack.Screen>
-                    
+          <stack.Screen name='Details' component={Details}></stack.Screen>        
 
         </stack.Navigator>
         
